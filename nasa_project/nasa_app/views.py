d8d7ed41793c0a60209303b9afe1b49348d13e3b
jerenteria <renteria.juan96@gmail.com>
2022-03-14 20:17:40 -0700

    trying really hard to get this working, need chick fil a break

--- nasa_project/nasa_app/views.py
+++ nasa_project/nasa_app/views.py
@@ -5,6 +5,8 @@ import requests, json, os
 load_dotenv()
 
 nasa_url = os.getenv("nasa_url")
+rover_photo = os.getenv("rover_photo")
+mars_weather = os.getenv("mars_weather")
 
 def get_data():
     response = requests.get(nasa_url)
@@ -12,8 +14,24 @@ def get_data():
     print(data)
     return(data)
 
+def get_photo():
+    response = requests.get(rover_photo)
+    photo = response.json()
+    print(photo)
+    return(photo)
+
+def get_mars_weather():
+    response = requests.get(mars_weather)
+    weather = response.json()
+    print(weather)
+    return(weather)
+
+
 def index(request):
     context = {
         'data' : get_data(),
+        'photo' : get_photo(),
+        'weather' : get_mars_weather(),
     }
     return render(request, "index.html", context)
+
