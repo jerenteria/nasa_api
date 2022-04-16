2a673aefc8ee704029ab827550b5f3cffbcdf91e
jerenteria <renteria.juan96@gmail.com>
2022-04-16 16:57:48 -0700

    deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn nasa_project.wsgi --log-file -
--- nasa_project/nasa_project/settings.py
+++ nasa_project/nasa_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mgcywgzgr_$y21$ocx@)%v-s(kel)s0kwx79du88qs%x&7=pf&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["blooming-sea-54604.herokuapp.com/", "127.0.0.1"]
 
 
 # Application definition
